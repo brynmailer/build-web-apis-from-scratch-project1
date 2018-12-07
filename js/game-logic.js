b855de5534cc0a48f7eb74c64955c31edef25a1e
brynmailer <bryndiupero@gmail.com>
2018-12-07 10:48:46 +1000

    first commit

---
+++ js/game-logic.js
@@ -0,0 +1,39 @@
+// player one move type
+const playerOneMoveOneType = undefined
+const playerOneMoveTwoType = undefined
+const playerOneMoveThreeType = undefined
+
+// player two move type
+const playerTwoMoveOneType = undefined
+const playerTwoMoveTwoType = undefined
+const playerTwoMoveThreeType = undefined
+
+// player one move values
+const playerOneMoveOneValue = undefined
+const playerOneMoveTwoValue = undefined
+const playerOneMoveThreeValue = undefined
+
+// player two move values
+const playerTwoMoveOneValue = undefined
+const playerTwoMoveTwoValue = undefined
+const playerTwoMoveThreeValue = undefined
+
+// functionality to set the player's moves
+const setPlayerMoves = (
+  player,
+  moveOneType,
+  moveOneValue,
+  moveTwoType,
+  moveTwoValue,
+  moveThreeType,
+  moveThreeValue
+) => {}
+
+// functionality to get the winner of the round
+const getRoundWinner = roundNumber => {}
+
+// functionality to get the winner of the game
+const getGameWinner = () => {}
+
+// functionality to set the computer's moves
+const setComputerMoves = () => {}
