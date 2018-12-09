0b2a585a2a97b37a849f3de442f90cee38a911e5
brynmailer <bryndiupero@gmail.com>
2018-12-09 14:37:50 +1000

    finished

--- js/game-logic.js
+++ js/game-logic.js
@@ -18,6 +18,18 @@ let playerTwoMoveOneValue
 let playerTwoMoveTwoValue
 let playerTwoMoveThreeValue
 
+// winners of each round
+let r1w
+let r2w
+let r3w
+
+// random int used to decide computer move types
+let randInt
+
+// player game score
+let P1Score = 0
+let P2Score = 0
+
 // constants called upon frequently
 const P1 = 'Player One'
 const P2 = 'Player Two'
@@ -73,56 +85,167 @@ const correctValues = (v1, v2, v3) =>
 
 // functionality to get the winner of the round
 const getRoundWinner = roundNumber => {
-  debugger
-  if (!checkRoundNumber(roundNumber)) {
+  switch (roundNumber) {
+    case 1:
+      return getMoveWinner(
+        playerOneMoveOneType,
+        playerOneMoveOneValue,
+        playerTwoMoveOneType,
+        playerTwoMoveOneValue
+      )
+
+    case 2:
+      return getMoveWinner(
+        playerOneMoveTwoType,
+        playerOneMoveTwoValue,
+        playerTwoMoveTwoType,
+        playerTwoMoveTwoValue
+      )
+
+    case 3:
+      return getMoveWinner(
+        playerOneMoveThreeType,
+        playerOneMoveThreeValue,
+        playerTwoMoveThreeType,
+        playerTwoMoveThreeValue
+      )
+
+    default:
+      return null
+  }
+}
+
+// functionality to get the winner of the move
+const getMoveWinner = (p1mt, p1mv, p2mt, p2mv) => {
+  if (!p1mt || !p1mv || !p2mt || !p2mv) {
     return null
   }
 
+  if (p1mt === p2mt) {
+    if (p1mv > p2mv) {
+      return P1
+    } else if (p1mv < p2mv) {
+      return P2
+    } else {
+      return TIE
+    }
+  }
+
+  if (p1mt === ROCK) {
+    return p2mt === SCISSORS ? P1 : p2mt === PAPER ? P2 : null
+  } else if (p1mt === SCISSORS) {
+    return p2mt === ROCK ? P2 : p2mt === PAPER ? P1 : null
+  } else if (p1mt === PAPER) {
+    return p2mt === ROCK ? P1 : p2mt === SCISSORS ? P2 : null
+  }
+}
+
+// functionality to get the winner of the game
+const getGameWinner = () => {
   if (
     !playerOneMoveOneType ||
     !playerOneMoveTwoType ||
     !playerOneMoveThreeType ||
-    !playerOneMoveOneValue ||
-    !playerOneMoveTwoValue ||
-    !playerOneMoveThreeValue ||
     !playerTwoMoveOneType ||
     !playerTwoMoveTwoType ||
     !playerTwoMoveThreeType ||
+    !playerOneMoveOneValue ||
+    !playerOneMoveTwoValue ||
+    !playerOneMoveThreeValue ||
     !playerTwoMoveOneValue ||
     !playerTwoMoveTwoValue ||
     !playerTwoMoveThreeValue
   ) {
     return null
   }
+
+  r1w = getRoundWinner(1)
+  r2w = getRoundWinner(2)
+  r3w = getRoundWinner(3)
+
+  zeroPlayerScore()
+
+  if (r1w === P1) {
+    P1Score++
+  } else if (r1w === P2) {
+    P2Score++
+  }
+
+  if (r2w === P1) {
+    P1Score++
+  } else if (r2w === P2) {
+    P2Score++
+  }
+
+  if (r3w === P1) {
+    P1Score++
+  } else if (r3w === P2) {
+    P2Score++
+  }
+
+  return P1Score > P2Score ? P1 : P1Score < P2Score ? P2 : TIE
 }
 
-// functionality to compare the move types involved in a round
-const compareTypes = (t1, t2) => {
-  return t1 === ROCK && t2 === SCISSORS
-    ? P1
-    : t1 === PAPER && t2 === ROCK
-      ? P1
-      : t1 === SCISSORS && t2 === PAPER
-        ? P1
-        : t1 === SCISSORS && t2 === ROCK
-          ? P2
-          : t1 === ROCK && t2 === PAPER
-            ? P2
-            : t1 === PAPER && t2 === SCISSORS
-              ? P2
-              : TIE
+// functionality to reset the player score after each game
+const zeroPlayerScore = () => {
+  P1Score = 0
+  P2Score = 0
 }
-
-// functionality to compare the move values involved in a round
-const compareValues = (v1, v2) => {
-  return v1 > v2 ? P1 : v1 < v2 ? P2 : TIE
-}
-
-// functionality to check if the round number is valid
-const checkRoundNumber = rn => rn > 0 && rn < 4
-
-// functionality to get the winner of the game
-const getGameWinner = () => {}
 
 // functionality to set the computer's moves
-const setComputerMoves = () => {}
+const setComputerMoves = () => {
+  generateMoveTypes()
+  generateMoveValues()
+}
+
+// functionality to generate a random integer
+const getRandomInt = max => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+// functionality to set the computer's move types
+const generateMoveTypes = () => {
+  randInt = getRandomInt(3)
+
+  if (randInt === 1) {
+    playerTwoMoveOneType = ROCK
+  } else if (randInt === 1) {
+    playerTwoMoveOneType = PAPER
+  } else {
+    playerTwoMoveOneType = SCISSORS
+  }
+
+  randInt = getRandomInt(3)
+
+  if (randInt === 0) {
+    playerTwoMoveTwoType = ROCK
+  } else if (randInt === 1) {
+    playerTwoMoveTwoType = PAPER
+  } else {
+    playerTwoMoveTwoType = SCISSORS
+  }
+
+  randInt = getRandomInt(3)
+
+  if (randInt === 0) {
+    playerTwoMoveThreeType = ROCK
+  } else if (randInt === 1) {
+    playerTwoMoveThreeType = PAPER
+  } else {
+    playerTwoMoveThreeType = SCISSORS
+  }
+}
+
+// functionality to set the computer's move values
+const generateMoveValues = () => {
+  debugger
+  playerTwoMoveOneValue = getRandomInt(97) + 1
+  playerTwoMoveTwoValue = getRandomInt(getNextMax(playerTwoMoveOneValue))
+  playerTwoMoveThreeValue = getRandomInt(
+    getNextMax(playerTwoMoveTwoValue + playerTwoMoveOneValue - 1)
+  )
+}
+
+const getNextMax = prevValue => {
+  return 99 - (prevValue - 1)
+}
